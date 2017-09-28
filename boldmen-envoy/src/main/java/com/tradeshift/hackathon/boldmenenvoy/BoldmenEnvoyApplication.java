e83d1f967a889fac65263a4dc01d78f2cae366ed
Andrei Serea <andrei.serea@capgemini.com>
2017-09-28 10:53:30 +0300

    services wrapper skeleton

---
+++ boldmen-envoy/src/main/java/com/tradeshift/hackathon/boldmenenvoy/BoldmenEnvoyApplication.java
@@ -0,0 +1,12 @@
+package com.tradeshift.hackathon.boldmenenvoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoldmenEnvoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoldmenEnvoyApplication.class, args);
+	}
+}
---
+++ boldmen-envoy/src/test/java/com/tradeshift/hackathon/boldmenenvoy/BoldmenEnvoyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tradeshift.hackathon.boldmenenvoy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoldmenEnvoyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
