becdab76c24df9d3dc205cff57cbc2ece4b0e6b9
Mihail Ionescu <mihail@mihail.ionescu>
2017-09-28 12:54:31 +0300

    Intercept calls

--- boldmen-envoy/src/main/java/com/tradeshift/hackathon/boldmenenvoy/controllers/DelegateController.java
+++ boldmen-envoy/src/main/java/com/tradeshift/hackathon/boldmenenvoy/controllers/DelegateController.java
@@ -2,12 +2,18 @@ package com.tradeshift.hackathon.boldmenenvoy.controllers;
 
 import java.text.ParseException;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.Context;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
@@ -16,10 +22,12 @@ public class DelegateController {
 
 	static Logger LOGGER = LoggerFactory.getLogger(DelegateController.class);
 
-	@RequestMapping(value = "/")
-    public String homePage() throws ParseException {
-        LOGGER.info("start page", DelegateController.class);
-    	return "Hello world";
+	
+	@RequestMapping(value = "/**")
+	@ResponseBody
+    public String homePage(HttpServletRequest req, HttpServletResponse res) throws ParseException {
+        LOGGER.info("URI: " + req.getRequestURI());
+    	return "dudu";
     }
 
 	@RequestMapping(value = "/delegate", method = RequestMethod.GET)
